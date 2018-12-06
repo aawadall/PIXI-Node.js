31da56572b5d3ae6d0d79642946b25eb0a5da183
Ahmed Awadallah <aawadall@ualberta.ca>
2018-12-06 18:18:07 -0500

    revert to original state

--- public/js/sprites.js
+++ public/js/sprites.js
@@ -1,20 +1,20 @@
 window.onload = function () {
-    import * as config from '../../package.json';
-    //const config = require('../../package.json');
-    let versionContainer = document.getElementById("version");
+    // import * as config from '../../package.json';
+    // //const config = require('../../package.json');
+    // let versionContainer = document.getElementById("version");
 
-    console.debug(config.version);
-    versionContainer.innerHTML = config.version;
+    // console.debug(config.version);
+    // versionContainer.innerHTML = config.version;
 
-    // The following line causes trouble, not sure how to fix it 
-    //import * as PIXI from 'pixi';
+    // // The following line causes trouble, not sure how to fix it 
+    // //import * as PIXI from 'pixi';
     
     
 
-    let app = new PIXI.Application({
-        width: 256,
-        height: 256
-    });
+    // let app = new PIXI.Application({
+    //     width: 256,
+    //     height: 256
+    // });
 
-    document.body.appendChild(app.view);
+    // document.body.appendChild(app.view);
 }

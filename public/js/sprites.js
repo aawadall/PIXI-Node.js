9e9fba0ffb6051a796ccc4c7f067657950647eec
Ahmed Awadallah <aawadall@ualberta.ca>
2018-12-06 16:53:24 -0500

    Changed index to ait for page

--- public/js/sprites.js
+++ public/js/sprites.js
@@ -1,6 +1,13 @@
-//import * as PIXI from './pixi'
-const PIXI = require('./pixi');
+window.onload = function () {
+    
+    // The following line causes trouble, not sure how to fix it 
+    import * as PIXI from './pixi';
+    
 
-let app = new PIXI.Application({width: 256, height: 256});
+    let app = new PIXI.Application({
+        width: 256,
+        height: 256
+    });
 
-document.body.appendChild(app.view);
+    document.body.appendChild(app.view);
+}
